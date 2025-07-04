4ece6262072b6ec9873be82f2212a534771debfa
V Play Games <vaibhavnargwani28@gmail.com>
2025-07-04 12:46:19 +0530

    Update minor inconsistencies

--- src/types/program.ts
+++ src/types/program.ts
@@ -1,10 +1,8 @@
 export interface Program {
   name: string;
   description: string;
-  detailedDescription?: string | null;
   website?: string | null;
   slack?: string | null;
-  slackChannel?: string | null;
   status: "active" | "ended" | "draft" | "indefinite" | "undefined" | "ditched";
   deadline?: string;
 }
